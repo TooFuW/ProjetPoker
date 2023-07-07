ab535783529cbd7f9ec73d3fd0c9154435ee3acd
Superpasteque213 <superpasteque7@gmail.com>
2023-07-07 11:35:07 +1100

    Implémentation (classe Deck Card) commit n°2

--- Deck.py
+++ Deck.py
@@ -1,5 +1,5 @@
-from deck_functions import *
-
+import random
+from Card import Card
 
 class Deck:
 
@@ -9,9 +9,71 @@ class Deck:
 
     def __init__(self) -> None:
         self.deck = new_deck()
+
+
     
     def __str__(self) -> str:
-        return str(self.deck)
+        string = "["
+        for card in self.deck:
+            string += str(card)+", "
+        string = string[0:-2]
+        string+="]"
+
+        return string
+    
+
+    def __len__(self):
+        return len(self.deck)
+    
+    
+
+    def draw(self):
+        """
+            Return the top card and remove it from the deck
+        """
+        top_card = self.deck[0]
+        self.deck.remove(top_card)
+        return(top_card)
+    
+
+    def shuffle(self):
+        """
+            Shuffle randomly and directly the deck 
+        """
+
+        random.shuffle(self.deck)
+    
+    
+
+
+
+
+### Fonctions utilisées dans les méthodes ###
+
+
+
+
+
+def new_deck():
+    """
+        Create a new deck of 52 non-shuffled cards
+    """
+    deck = []
+    suits = ["club","heart","spade","diamond"]
+    ranks = ["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"]
+
+    for suit in suits:
+        for rank in ranks:
+            deck.append(Card(suit,rank))
+
+    return deck
+
+
+
+
+
+
+
         
 
 
--- class_tester.py
+++ class_tester.py
@@ -3,3 +3,10 @@ from Deck import Deck
 my_deck = Deck()
 
 print(my_deck)
+print("\n\n")
+
+my_deck.shuffle()
+
+print(my_deck, len(my_deck))
+print("\n\n")
+print(my_deck.draw(), len(my_deck))
