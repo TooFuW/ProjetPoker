5dde7dce3c31f7084df397d103fa12436780d44e
Superpasteque213 <superpasteque7@gmail.com>
2023-07-11 09:17:37 +1100

    commit n°1 fonction str_to_list

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #Menu du jeu Poker
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#fonction importante pour plus tard à laisser en bas 
+def str_to_list(string: str):   #fonctionne seulement sur une liste supposée valide
+    liste = []
+    string = string.removeprefix("[")
+    string = string.removesuffix("]")
+
+    return string.split(",")
