16cdd41892a4504727264b10c929016f2a3b36c6
Superpasteque213 <superpasteque7@gmail.com>
2023-07-11 09:45:42 +1100

    commit n°10

---
+++ main.py
@@ -0,0 +1,32 @@
+from socket import *
+
+
+class Main:
+    """
+        on proggram start
+    """
+
+    def __init__(self) -> None:  #initialise les variables principales
+        self.lobbys = []
+        self.lobbys = []
+
+    def start(self):   #lance l'écoute serveur et le script
+        pass
+
+
+
+
+
+
+def send_packet(packet : str, conn : socket):
+    pass
+
+def send_lobbys(conn : socket, lobbys : list):
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.start()
