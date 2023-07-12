3df104c6367fe0aeb8d112bb7c7b98687ea09442
Eyrian Muet <eyrianmuet@gmail.com>
2023-07-12 18:48:36 +1100

    Commit 1.3 : Création d'une classe HUD_State pour gérer l'interface actuelle. De cette façon je peux créer autant d'interfaces que je veux et afficher seulement une à la fois. J'ai adapté le code, fais quelques ajustements et tout documenté !

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class Button:
         """
         # Attributs principaux
         self.pressed = False
+        self.text = text
         # self.elevation sert à garder la valeur par défaut de l'élévation, on va plutôt utiliser self.dynamic_elevation dans le code
         self.elevation = elevation
         self.dynamic_elevation = elevation
@@ -73,13 +74,19 @@ class Button:
                 self.pressed = True
             # On fait les actions souhaitées lorsque le clic est relaché
             else:
+                # CODE POUR QUAND LE BOUTON EST CLIQUE
                 self.dynamic_elevation = self.elevation
                 if self.pressed == True:
-                    # CODE POUR QUAND LE BOUTON EST CLIQUE
-                    print("click")
                     self.pressed = False
-                    pygame.quit()
-                    sys.exit()
+                    if self.text == "EXIT":
+                        pygame.quit()
+                        sys.exit()
+                    elif self.text == "PLAY":
+                        game_state.state = "Lobby Menu"
+                    elif self.text == "BACK":
+                        game_state.state = "Main Menu"
+                    else:
+                        print(self.text)
         # Le else est là pour reset l'état du bouton lorsqu'il n'y a plus aucune interaction
         else:
             self.dynamic_elevation = self.elevation
@@ -87,22 +94,81 @@ class Button:
             self.pressed = False
 
 
+class HUD_State:
+    """Classe HUD_State créée pour gérer l'interface active (https://www.youtube.com/watch?v=j9yMFG3D7fg)
+    """
+
+    def __init__(self):
+        """Initialisation de l'état de l'interface
+        """
+        # self.state définit l'état actuel de l'interface (qui est par défaut Main Menu)
+        self.state = "Main Menu"
+    
+    def mainmenu(self):
+        """mainmenu est la fonction qui fait tourner/afficher le menu principal
+        """
+        # Rassemblement de tout les événements
+        for event in pygame.event.get():
+            pass
+
+        # Dessin l'image de fond sur la surface de l'écran
+        screen.blit(fond, (0, 0))
+
+        # Affichage des bouttons
+        accountbutton.draw()
+        # Cliquer sur le bouton PLAY ouvre l'interface présentant les lobbys disponibles
+        playbutton.draw()
+        settingsbutton.draw()
+        # Cliquer sur le bouton EXIT ferme la fenêtre purement et simplement
+        exitbutton.draw()
+
+        # Met à jour l'affichage de l'interface
+        pygame.display.update()
+    
+    def lobbymenu(self):
+        """lobbymenu est la fonction qui fait tourner/afficher le menu des lobbys
+        """
+        # Rassemblement de tout les événements
+        for event in pygame.event.get():
+            pass
+
+        # Dessin l'image de fond sur la surface de l'écran
+        screen.blit(fond, (0, 0))
+
+        # Affichage des bouttons
+        # Cliquer sur le bouton EXIT ferme la fenêtre purement et simplement
+        backbutton.draw()
+
+        # Met à jour l'affichage de l'interface
+        pygame.display.update()
+
+    def state_manager(self):
+        """state_manager se charge d'afficher la bonne interface en fonction de l'état de l'interface
+        """
+        if self.state == "Main Menu":
+            self.mainmenu()
+        elif self.state == "Lobby Menu":
+            self.lobbymenu()
+
+
 # Pygame setup
 pygame.init()
+game_state = HUD_State()
 # Taille de la fenêtre
 screen_info = pygame.display.Info()
 screen_width = screen_info.current_w
 screen_height = screen_info.current_h
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 # Nom de la fenêtre
-pygame.display.set_caption("Main Menu")
+pygame.display.set_caption("Menu Jeu Poker")
 clock = pygame.time.Clock()
 
 # Chargement de l'image de fond
 fond = pygame.image.load("PokerBackground.jpg")
 fond = pygame.transform.scale(fond, (screen_width, screen_height))
 
-#Création de l'objet accountbutton
+# Création de tout les boutons utilisés
+# Création de l'objet accountbutton
 accountbutton = Button("ACCOUNT", "Roboto", 30, 150, 75, ((screen_width - 170), (20)), 3)
 # Création de l'objet playbutton
 playbutton = Button("PLAY", "Roboto", 100, 425, 100, ((screen_width // 2)-(425//2), (screen_height // 2) - 30), 6)
@@ -110,30 +176,30 @@ playbutton = Button("PLAY", "Roboto", 100, 425, 100, ((screen_width // 2)-(425//
 settingsbutton = Button("SETTINGS", "Roboto", 100, 425, 100, ((screen_width // 2)-(425//2), (screen_height // 2) + 100), 6)
 # Création de l'objet quitbutton
 exitbutton = Button("EXIT", "Roboto", 100, 425, 100, ((screen_width // 2)-(425//2), (screen_height // 2) + 230), 6)
+# Création de l'objet backbutton
+backbutton = Button("BACK", "Roboto", 100, 425, 100, ((screen_width // 2)-(425//2), (screen_height // 2) + 230), 6)
 
 # Gameloop
 while True:
-    # Rassemblement de tout les événements
-    for event in pygame.event.get():
-        # pygame.QUIT event veut dire que l'utilisateur a cliqué sur la croix pour fermer la fenêtre
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    # Dessin l'image de fond sur la surface de l'écran
-    screen.blit(fond, (0, 0))
-
-    # Affichage des bouttons
-    accountbutton.draw()
-    playbutton.draw()
-    settingsbutton.draw()
-    exitbutton.draw()
-
-    # Met à jour l'affichage de l'interface
-    pygame.display.update()
-
+    # Cet appel permet de gérer l'interface active
+    game_state.state_manager()
     #Limite les FPS à 60
     clock.tick(60)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
