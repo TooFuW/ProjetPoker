71f7aa90b1b4d67eab4adaa9ecee32182df0f06e
Eyrian Muet <eyrianmuet@gmail.com>
2023-07-28 12:00:04 +1100

    Commit 1.25 : Réglage des problèmes d'affichage avec la scrollbox

--- Test pygame.py
+++ Test pygame.py
@@ -51,7 +51,7 @@ class Button:
 
         # Button text
         self.text = text
-        gui_font = pygame.font.SysFont(police, textsize, False, False)
+        gui_font = pygame.font.SysFont(police, width_scale(textsize), False, False)
         self.text_surf = gui_font.render(text, True, "#FFFFFF")
         self.text_rect = self.text_surf.get_rect(center = self.top_rect.center)
 
@@ -59,7 +59,7 @@ class Button:
         self.image = None
         if image != None:
             self.image = pygame.image.load(image)
-            self.image = pygame.transform.scale(self.image, (width, height))
+            self.image = pygame.transform.scale(self.image, (width_scale(width), height_scale(height)))
 
     def draw(self):
         """Génération/affichage du bouton
@@ -147,14 +147,17 @@ class ScrollBox:
             height (int): Hauteur de la scrollbox
             servers (list): Liste des serveurs/tables à afficher
         """
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        self.x = width_scale(x)
+        self.default_x = x
+        self.y = height_scale(y)
+        self.default_y = y
+        self.width = width_scale(width)
+        self.default_width = width
+        self.height = height_scale(height)
         self.servers = servers
         self.scroll_pos = 0
         self.indentation = "          "# Len = 10
-        self.hauteurbox = 50
+        self.hauteurbox = height_scale(50)
         self.selected = False
 
     def draw(self):
@@ -177,10 +180,10 @@ class ScrollBox:
 
         # Dessin des éléments visibles
         for i, server in enumerate(self.servers[self.scroll_pos:]):
-            item_y = self.y + item_offset_y
+            item_y = self.default_y + item_offset_y
             # Délimitation de la zone de la scrollbox
             text = (server[0] + self.indentation + str(server[1]) + self.indentation + server[2] + self.indentation + server[3] + self.indentation + server[4] + self.indentation + server[5])
-            item_rect = Button("server", text, "Roboto", 24, "#475F77", "#354B5E", self.width, self.hauteurbox, (self.x, item_y), 3, 0)
+            item_rect = Button("server", text, "Roboto", 24, "#475F77", "#354B5E", self.default_width, self.hauteurbox, (self.default_x, item_y), 3, 0)
             item_rect.check_click()
             mouse_pos = pygame.mouse.get_pos()
             if pygame.mouse.get_pressed()[0]:
@@ -235,7 +238,7 @@ class TextInputBox:
         self.user_text = ""
         self.max_caracteres = max_caracteres
         # Position du texte
-        self.input_rect = pygame.Rect(pos[0], pos[1], width, height)
+        self.input_rect = pygame.Rect(width_scale(pos[0]), height_scale(pos[1]), width_scale(width), height_scale(height))
         # Couleur de la box en fonction si elle est sélecionnée ou non
         self.color_active = active_color
         self.color_passive = passive_color
@@ -286,16 +289,17 @@ class Preview_Table:
             pos (tuple): Position x, y de la preview
             scale (float) = 1: Multiplicateur de taille de la preview (1 par défaut)
         """
-        self.pos = pos
-        self.width = 500*scale
-        self.height = 500*scale
+        self.x = width_scale(pos[0])
+        self.y = height_scale(pos[1])
+        self.width = width_scale(500*scale)
+        self.height = height_scale(500*scale)
 
     def draw(self):
         """Génération/affichage de la preview
         """
         
         # Dessinez la zone de la preview sur l'écran
-        pygame.draw.rect(screen, "#006400", ((screen_width // 2) + (700 // 2), (screen_height // 2) - (650 // 2), self.width, self.height), border_radius = 10)
+        pygame.draw.rect(screen, "#006400", (self.x, self.y, self.width, self.height), border_radius = 10)
 
 
 
@@ -324,7 +328,7 @@ class HUD_State:
         # Dessine l'image de fond sur le screen de l'écran
         screen.blit(fond, (0, 0))
         # Dessine le logo du jeu
-        screen.blit(logojeu, ((screen_width // 2) - 125, (screen_height // 2) - 500))
+        screen.blit(logojeu, (width_scale(800), height_scale(40)))
         # Dessine le logo MWTE
         screen.blit(logomwte, logomwte_rect)
 
@@ -354,8 +358,8 @@ class HUD_State:
         # Affichage des chips de l'utilisateur à droite du bouton ACCOUNT
         gui_font = pygame.font.SysFont("Roboto", 40)
         text_surf = gui_font.render("Chips : ", True, "#FFFFFF")
-        pygame.draw.rect(screen, "#475F77", pygame.Rect(((screen_width // 2) + 580, (screen_height // 2) - 510), (200, 50)), border_radius = 3)
-        screen.blit(text_surf, ((screen_width // 2) + 590, (screen_height // 2) - 500))
+        pygame.draw.rect(screen, "#475F77", pygame.Rect((width_scale(1540), height_scale(30)), (width_scale(200), height_scale(50))), border_radius = 3)
+        screen.blit(text_surf, (width_scale(1550), height_scale(40)))
         # Cliquer sur le bouton EXIT ferme la fenêtre purement et simplement
         exitbutton.draw()
         # Cliquer sur le bouton GAMES HISTORY affiche l'historique des parties
@@ -424,15 +428,15 @@ class HUD_State:
         # Affichage des chips de l'utilisateur à droite du bouton ACCOUNT
         gui_font = pygame.font.SysFont("Roboto", 40)
         text_surf = gui_font.render("Chips : ", True, "#FFFFFF")
-        pygame.draw.rect(screen, "#475F77", pygame.Rect(((screen_width // 2) + 580, (screen_height // 2) - 510), (200, 50)), border_radius = 3)
-        screen.blit(text_surf, ((screen_width // 2) + 590, (screen_height // 2) - 500))
+        pygame.draw.rect(screen, "#475F77", pygame.Rect((width_scale(1540), height_scale(30)), (width_scale(200), height_scale(50))), border_radius = 3)
+        screen.blit(text_surf, (width_scale(1550), height_scale(40)))
 
         # On crée la box dans laquelle on pourra écrire un code de partie pour rejoindre
         tablecodeinput.draw()
         # On affiche un texte au-dessus de la box qui indique ce que cette dernière fait
         gui_font = pygame.font.SysFont("Roboto", 50)
         text_surf = gui_font.render("Private Table Code", True, "#000000")
-        text_rect = text_surf.get_rect(center = pygame.Rect(((screen_width // 2) + 485, (screen_height // 2) + 195), (150, 75)).center)
+        text_rect = text_surf.get_rect(center = pygame.Rect((width_scale(1445), height_scale(735)), (width_scale(150), height_scale(75))).center)
         screen.blit(text_surf, text_rect)
 
         # On crée la preview des tables
@@ -461,8 +465,8 @@ class HUD_State:
         # Affichage des chips de l'utilisateur à droite du bouton ACCOUNT
         gui_font = pygame.font.SysFont("Roboto", 40)
         text_surf = gui_font.render("Chips : ", True, "#FFFFFF")
-        pygame.draw.rect(screen, "#475F77", pygame.Rect(((screen_width // 2) + 580, (screen_height // 2) - 510), (200, 50)), border_radius = 3)
-        screen.blit(text_surf, ((screen_width // 2) + 590, (screen_height // 2) - 500))
+        pygame.draw.rect(screen, "#475F77", pygame.Rect((width_scale(1540), height_scale(30)), (width_scale(200), height_scale(50))), border_radius = 3)
+        screen.blit(text_surf, (width_scale(1550), height_scale(40)))
 
         # Affichage des boutons de pages de paramètres
         settingpage1button.draw()
@@ -471,24 +475,24 @@ class HUD_State:
         # Affichage des pages de paramètres
         transparent_surface = pygame.Surface((1500, 850), pygame.SRCALPHA)
         pygame.draw.rect(transparent_surface, (0, 0, 0, 128), (0, 0, 1500, 850), border_radius = 5)
-        screen.blit(transparent_surface, ((screen_width // 2) - 700, (screen_height // 2) - 380))
-        screen.blit(transparent_surface, ((screen_width // 2) - 700, (screen_height // 2) - 380))
-        screen.blit(transparent_surface, ((screen_width // 2) - 700, (screen_height // 2) - 380))
+        screen.blit(transparent_surface, (width_scale(260), height_scale(160)))
+        screen.blit(transparent_surface, (width_scale(260), height_scale(160)))
+        screen.blit(transparent_surface, (width_scale(260), height_scale(160)))
         # Page 1
         if self.setting_page == 1:
             gui_font = pygame.font.SysFont("Roboto", 40)
             settingtext_surf = gui_font.render("Page 1 des paramètres", True, "#FFFFFF")
-            screen.blit(settingtext_surf, ((screen_width // 2) - 690, (screen_height // 2) - 370))
+            screen.blit(settingtext_surf, (width_scale(270), height_scale(170)))
         # Page 2
         elif self.setting_page == 2:
             gui_font = pygame.font.SysFont("Roboto", 40)
             settingtext_surf = gui_font.render("Page 2 des paramètres", True, "#FFFFFF")
-            screen.blit(settingtext_surf, ((screen_width // 2) - 690, (screen_height // 2) - 370))
+            screen.blit(settingtext_surf, (width_scale(270), height_scale(170)))
         # Page 3
         elif self.setting_page == 3:
             gui_font = pygame.font.SysFont("Roboto", 40)
             settingtext_surf = gui_font.render("Page 3 des paramètres", True, "#FFFFFF")
-            screen.blit(settingtext_surf, ((screen_width // 2) - 690, (screen_height // 2) - 370))
+            screen.blit(settingtext_surf, (width_scale(270), height_scale(170)))
 
         # Met à jour l'affichage de l'interface
         pygame.display.update()
@@ -515,18 +519,18 @@ class HUD_State:
         # Affichage des chips de l'utilisateur
         gui_font = pygame.font.SysFont("Roboto", 40)
         text_surf = gui_font.render("Chips : ", True, "#FFFFFF")
-        pygame.draw.rect(screen, "#475F77", pygame.Rect(((screen_width // 2) - 560, (screen_height // 2) - 75), (225, 50)), border_radius = 3)
-        screen.blit(text_surf, ((screen_width // 2) - 550, (screen_height // 2) - 65))
+        pygame.draw.rect(screen, "#475F77", pygame.Rect((width_scale(400), height_scale(465)), (width_scale(225), height_scale(50))), border_radius = 3)
+        screen.blit(text_surf, (width_scale(410), height_scale(475)))
         # Affichage du pseudo de l'utilisateur
         gui_font = pygame.font.SysFont("Roboto", 60)
         text_surf = gui_font.render("PSEUDO", True, "#FFFFFF")
-        pygame.draw.rect(screen, "#475F77", pygame.Rect(((screen_width // 2) - 275, (screen_height // 2) - 350), (600, 100)), border_radius = 3)
-        screen.blit(text_surf, ((screen_width // 2) - 265, (screen_height // 2) - 320))
+        pygame.draw.rect(screen, "#475F77", pygame.Rect((width_scale(685), height_scale(190)), (width_scale(600), height_scale(100))), border_radius = 3)
+        screen.blit(text_surf, (width_scale(695), height_scale(220)))
         # Affichage des infos de l'utilisateur
         gui_font = pygame.font.SysFont("Roboto", 60)
         text_surf = gui_font.render("INFORMATIONS", True, "#FFFFFF")
-        pygame.draw.rect(screen, "#475F77", pygame.Rect(((screen_width // 2) - 275, (screen_height // 2) - 225), (600, 650)), border_radius = 3)
-        screen.blit(text_surf, ((screen_width // 2) - 265, (screen_height // 2) - 215))
+        pygame.draw.rect(screen, "#475F77", pygame.Rect((width_scale(685), height_scale(315)), (width_scale(600), height_scale(650))), border_radius = 3)
+        screen.blit(text_surf, (width_scale(695), height_scale(325)))
         # Affichage du bouton de déconnexion de l'utilisateur
         deconnexionbutton.draw()
         # Affichage du bouton de paramètres du compte de l'utilisateur
@@ -568,8 +572,8 @@ class HUD_State:
         # Affichage des chips de l'utilisateur à droite du bouton ACCOUNT
         gui_font = pygame.font.SysFont("Roboto", 40)
         text_surf = gui_font.render("Chips : ", True, "#FFFFFF")
-        pygame.draw.rect(screen, "#475F77", pygame.Rect(((screen_width // 2) + 580, (screen_height // 2) - 510), (200, 50)), border_radius = 3)
-        screen.blit(text_surf, ((screen_width // 2) + 590, (screen_height // 2) - 500))
+        pygame.draw.rect(screen, "#475F77", pygame.Rect((width_scale(1540), height_scale(30)), (width_scale(200), height_scale(50))), border_radius = 3)
+        screen.blit(text_surf, (width_scale(1550), height_scale(40)))
 
         # Met à jour l'affichage de l'interface
         pygame.display.update()
@@ -590,10 +594,26 @@ class HUD_State:
 
 
 def width_scale(largeur : int):
+    """Fonction qui permet de donner une largeur adaptée à la largeur de l'écran (par rapport à un écran de 17 pouces/1920px)
+
+    Args:
+        largeur (int): largeur à transformer
+
+    Returns:
+        width (int): largeur adaptée
+    """
     width = largeur*largeur_actuelle//1920
     return width
 
 def height_scale(hauteur : int):
+    """Fonction qui permet de donner une hauteur adaptée à la hauteur de l'écran (par rapport à un écran de 17 pouces/1080px)
+
+    Args:
+        hauteur (int): hauteur à transformer
+
+    Returns:
+        height (int): hauteur adaptée
+    """
     height = hauteur*hauteur_actuelle//1080
     return height
 
@@ -621,17 +641,17 @@ fond = pygame.transform.scale(pokertablebackground, (screen_width, screen_height
 
 # Chargement du logo du jeu
 logojeu = pygame.image.load("PokerBackground.jpg")
-logojeu = pygame.transform.scale(logojeu, (250, 250))
+logojeu = pygame.transform.scale(logojeu, (width_scale(250), height_scale(250)))
 
 # Chargement du logo MWTE
 logomwte = pygame.image.load("logo mwte.jpg")
-logomwte = pygame.transform.scale(logomwte, (175, 175))
+logomwte = pygame.transform.scale(logomwte, (width_scale(175), height_scale(175)))
 logomwte_rect = logomwte.get_rect()
-logomwte_rect.topleft = ((screen_width // 2) - (1900 // 2), (screen_height // 2) + (700 // 2))
+logomwte_rect.topleft = (width_scale(10), height_scale(890))
 
 # Chargement de la photo de profil du joueur
 pdpplayer = pygame.image.load("logo mwte.jpg")
-pdpplayer = pygame.transform.scale(pdpplayer, (300, 300))
+pdpplayer = pygame.transform.scale(pdpplayer, (width_scale(300), height_scale(300)))
 
 # Création de tout les boutons utilisés
 # Création de l'objet accountbutton
