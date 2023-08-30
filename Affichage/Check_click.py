2de5294c2c5d81af74c17bd3b695c815e546b5bd
Eyrian Muet <eyrianmuet@gmail.com>
2023-08-30 15:30:57 +1100

    Commit 1.49.5.1 Réglage urgent d'un crash qui arrivait avec la preview si le serveur n'était pas activé

--- Affichage/Check_click.py
+++ Affichage/Check_click.py
@@ -101,7 +101,6 @@ def check_click(Button):
                 if Global_objects.game_state.state == "Lobby Menu":
                     Global_objects.game_state.server_test = Button.text
                     Global_objects.game_state.table_selected = Button.text.split("          ")
-                    
                     lobby_id = int(Global_objects.game_state.table_selected[-1])
                     ask_sits_infos(Global_objects.client_socket,lobby_id)
             # Lorsque le bouton JOIN est cliqué pour rejoindre la table sélectionnée
