01decae63a6cc1d5cc6b49c942fa63b189c95210
Eyrian Muet <eyrianmuet@gmail.com>
2023-09-07 16:56:55 +1100

    Commit 1.57.1 : Réglage du bug de la scrollbox qui n'affichait pas mais créait quand même des boutons cliquables sous la scrollbox !

--- Affichage/Scrollbox_class.py
+++ Affichage/Scrollbox_class.py
@@ -42,47 +42,47 @@ class ScrollBox:
         self.selected = False
         # Référencement du serveur sélectionné
         self.server_selected = None
+        # Calcul de la zone d'affichage des éléments
+        self.display_area = pygame.Rect(self.x, self.y- height_scale(3, self.hauteur_actuelle), self.width, self.height + height_scale(15, self.hauteur_actuelle))
 
     def draw(self):
         """Génération/affichage de la scrollbox
         """
         # Créez une surface transparente
-        transparent_surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
-        
+        transparent_surface = pygame.Surface((self.width, self.height + height_scale(15, self.hauteur_actuelle)), pygame.SRCALPHA)
+
         # Dessinez le rectangle transparent sur la surface
-        pygame.draw.rect(transparent_surface, (0, 0, 0, 128), (0, 0, self.width, self.height))
+        pygame.draw.rect(transparent_surface, (0, 0, 0, 128), (0, 0, self.width, self.height + height_scale(15, self.hauteur_actuelle)))
         
         # Afficher la surface transparente sur l'écran
-        self.screen.blit(transparent_surface, (self.x, self.y))
-
-        # Calcul de la zone d'affichage des éléments
-        self.display_area = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.screen.blit(transparent_surface, (self.x, self.y - height_scale(3, self.hauteur_actuelle)))
 
         # Décalage initial
         item_offset_y = 0
 
         # Dessin des éléments visibles
-        for _, server in enumerate(self.servers[self.scroll_pos:]):
-            item_y = self.default_y + item_offset_y
-            # Délimitation de la zone de la scrollbox
-            text = (server[0] + self.indentation + str(server[1]) + self.indentation + server[2] + self.indentation + server[3] + self.indentation + server[4] + self.indentation + server[5])
-            item_rect = Button_class.Button(self.largeur_actuelle, self.hauteur_actuelle, self.screen, "server", text, "Roboto", 24, "#475F77", "#354B5E", "#D74B4B", "#354B5E", self.default_width, self.hauteurbox, (self.default_x, item_y), 3, 0)
-            item_rect.check_click()
-            mouse_pos = pygame.mouse.get_pos()
-            # Quand un bouton de serveur est cliqué
-            if item_rect.top_rect.collidepoint(mouse_pos):
-                if pygame.mouse.get_pressed()[0]:
-                    self.selected = True
-                    self.server_selected = item_rect
-                else:
-                    if self.selected == True:
-                        self.selected = False
-                        Check_click.check_click(self.server_selected)
-            # Affichage des serveurs disponibles
-            if item_rect.top_rect.colliderect(self.display_area):
-                item_rect.draw()
-            # Ajouter un padding entre chaque serveur
-            item_offset_y += self.hauteurbox + 10  # Ajouter 10 pixels de padding
+        for i, server in enumerate(self.servers[self.scroll_pos:]):
+            if i < 13:
+                item_y = self.default_y + item_offset_y
+                # Délimitation de la zone de la scrollbox
+                text = (server[0] + self.indentation + str(server[1]) + self.indentation + server[2] + self.indentation + server[3] + self.indentation + server[4] + self.indentation + server[5])
+                item_rect = Button_class.Button(self.largeur_actuelle, self.hauteur_actuelle, self.screen, "server", text, "Roboto", 24, "#475F77", "#354B5E", "#D74B4B", "#354B5E", self.default_width, self.hauteurbox, (self.default_x, item_y), 3, 0)
+                item_rect.check_click()
+                mouse_pos = pygame.mouse.get_pos()
+                # Quand un bouton de serveur est cliqué
+                if item_rect.top_rect.collidepoint(mouse_pos):
+                    if pygame.mouse.get_pressed()[0]:
+                        self.selected = True
+                        self.server_selected = item_rect
+                    else:
+                        if self.selected == True:
+                            self.selected = False
+                            Check_click.check_click(self.server_selected)
+                # Affichage des serveurs disponibles
+                if item_rect.top_rect.colliderect(self.display_area):
+                    item_rect.draw()
+                # Ajouter un padding entre chaque serveur
+                item_offset_y += self.hauteurbox + 10  # Ajouter 10 pixels de padding
 
     def scroll_up(self):
         """Pour scroller vers le haut
